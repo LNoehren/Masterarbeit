fixed normalization params

scripts/compute_norm_params.py
@@ -1,21 +1,25 @@
 import os
-import cv2
 import numpy as np
+from utils import read_image
 
 
 dataset_path = "/home/lennard/Datasets/vocalfolds-master/img"
 
 
-all_data = []
+sum = 0
+sumsquare = 0
+count = 0
 for root, dirs, files in os.walk(dataset_path):
     for file in files:
-        data = cv2.imread(root + "/" + file, -1)
-        all_data.append(np.reshape(data, (-1, 3)))
+        data = read_image(root + "/" + file)
+        sum += np.sum(data, axis=(0, 1))
+        sumsquare += np.sum(data.astype(int)**2, axis=(0, 1))
+        count += np.prod(data.shape[:2])
 
-all_data = np.reshape(np.stack(all_data), (-1, 3))
-mean = np.mean(all_data, axis=0)
-std = np.std(all_data, axis=0)
+mean = sum / count
+std = np.sqrt(sumsquare / count - (mean**2))
 
 print("mean: {} std: {}".format(mean, std))
 
-# vocalfolds: mean: [ 72.23536678  92.29408995 160.99123017] std: [43.67935975 46.16065498 59.28851215]
+# vocalfolds: mean: [160.99123017  92.29408995  72.23536678] std: [59.28851215 46.16065498 43.67935978]
+# cityscapes: mean: [72.45996064 82.30920661 71.8585933 ] std: [46.99697658 47.7633243  46.96281819]